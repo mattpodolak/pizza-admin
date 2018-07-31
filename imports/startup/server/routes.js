8e669342e707b8e6d4772d0e4a43a82a04db180d
mattpodolak <mpodola2@gmail.com>
2018-07-31 12:25:13 -0400

    added a GET param for show

--- imports/startup/server/routes.js
+++ imports/startup/server/routes.js
@@ -1,17 +1,20 @@
 import { Router } from 'meteor/iron:router'
 import { CustomerCollection } from '../../api/customers.js'
 
-Router.route('/show', function () {
+Router.route('/show/:_phone', function () {
+    var params = this.params
     var req = this.request;
     var res = this.response;
-    var customers = CustomerCollection.find()
-    res.end(customers);
+    var phoneNum = params._phone
+    var customers = CustomerCollection.find({phone: phoneNum})
+    console.log('Customer ', customers)
+    res.end('hello');
 }, {where: 'server'});
 
 Router.route('/add', function () {
     var req = this.request;
     var res = this.response;
-    var first_name = 'John', last_name= 'Smitherson', phone= '1234567890', address_one= '123 Fake St', address_two= '', postal_code= 'A1B2C3', city= 'Fakeland';
+    var first_name = 'John', last_name= 'Smitherson', phone= '1234567890', address_one= '123 Fake St', address_two= '', postal_code= 'A1B2C3', city= 'Fakeland', user='Napoli';
     Meteor.call('customerCollection.insert', first_name, last_name, phone, address_one, address_two, postal_code, city);
     res.end('hello from the server\n');
 }, {where: 'server'});
