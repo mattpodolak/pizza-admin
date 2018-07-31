4e34150cad9c8801b97b61f7989751d04aef342d
mattpodolak <mpodola2@gmail.com>
2018-07-31 15:34:24 -0400

    POST request works

--- imports/startup/server/routes.js
+++ imports/startup/server/routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'meteor/iron:router'
 import { CustomerCollection } from '../../api/customers.js'
 
-Router.route('/show/:_user/:_phone', function () {
-    var params = this.params
-    var req = this.request;
-    console.log(req)
-    var res = this.response;
-    var phoneNum = params._phone
-    var userName = params._user
-    var customer = CustomerCollection.findOne({phone: phoneNum, user: userName})
-    console.log('Customer ', customer)
-    res.end(customer);
-}, {where: 'server'});
+// Router.route('/show/:_user/:_phone', function () {
+//     var params = this.params
+//     var req = this.request;
+//     console.log(req)
+//     var res = this.response;
+//     var phoneNum = params._phone
+//     var userName = params._user
+//     var customer = CustomerCollection.findOne({phone: phoneNum, user: userName})
+//     console.log('Customer ', customer)
+//     res.end(customer);
+// }, {where: 'server'});
 
 Router.route('/add', function () {
     var req = this.request;
