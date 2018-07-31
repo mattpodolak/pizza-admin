f368106e6719511a422609e034784696c32862d8
mattpodolak <mpodola2@gmail.com>
2018-07-31 14:47:33 -0400

    added api to check customer info via GET

--- imports/api/customers.js
+++ imports/api/customers.js
@@ -2,15 +2,36 @@
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
+import { Restivus } from 'meteor/nimble:restivus'
 
 export const CustomerCollection = new Mongo.Collection('customerCollection');
 
 if (Meteor.isServer) {
     // This code only runs on the server
-    Meteor.publish('customerCollection', function customerCollectionPublication() {
-      return CustomerCollection.find();
+    // Meteor.publish('customerCollection', function customerCollectionPublication() {
+    //   return CustomerCollection.find();
+    // });
+    // Global API configuration
+    var Api = new Restivus({
+      useDefaultAuth: true,
+      prettyJson: true
+    });
+
+    // Maps to: /api/articles/:id
+    Api.addRoute('check/:user/:phone', {authRequired: false}, {
+      get: function () {
+        var phoneNum = this.urlParams.phone
+        var userName = this.urlParams.user
+        var customer =  CustomerCollection.findOne({phone: phoneNum, user: userName})
+        if(customer == null){
+          return {"first_name": null}
+        }
+        return customer
+      }
     });
   }
+
+
 
 Meteor.methods({
     'customerCollection.insert'(first_name, last_name, phone, address_one, address_two, postal_code, city, user) {
